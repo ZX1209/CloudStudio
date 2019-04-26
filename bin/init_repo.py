732770e39d2623ca51ef27941cdc0f585727c8b5
GL <1404919041@qq.com>
2019-04-26 17:41:16 +0800

    finished init_repo; and fix the #! problem now python reference to python3 in my world and python2 means python2

---
+++ bin/init_repo.py
@@ -0,0 +1,54 @@
+#!python
+import argparse
+import sys
+from clara import ThirdShell
+import requests
+from pathlib import Path
+import logging as log
+
+log.basicConfig(level=log.INFO)
+log.debug('this is a demo massage')
+
+cwd = Path.cwd()
+# #
+# cmd_atgs_list = " add ".split()
+
+# setup
+parser = argparse.ArgumentParser()
+parser.add_argument("userName")
+parser.add_argument("userPass")
+# type choices action default...
+
+
+def init_repo(args=None):
+    """main
+    """
+    # args parser
+    # default is sys.argv
+    # can be a [str]
+    args = parser.parse_args(args)
+
+    # init commit
+    ThirdShell.runCmdStrs([
+        "git init", "git add --all",
+        "git commit -m 'auto commmit for init a github repo'"
+    ])
+
+    # create a repo
+    log.info('start request')
+    response = requests.post('https://api.github.com/user/repos',
+                             json={'name': cwd.name},
+                             auth=(args.userName, args.userPass))
+
+    if response:
+        ThirdShell.runCmdStrs([
+            f"git remote add origin https://github.com/ZX1209/{cwd.name}.git",
+            'git push --set-upstream origin master'
+        ])
+        log.info('success')
+    else:
+        log.error(response.json())
+
+
+if __name__ == "__main__":
+    init_repo()
--- bin/tree.py
+++ bin/tree.py
@@ -1,14 +1,16 @@
-#! python3
+#!python
 from pathlib import Path
 
-cwd  = Path.cwd()
+cwd = Path.cwd()
 
-def tree(path,depth=0):
+
+def tree(path, depth=0):
     for child in path.iterdir():
         if child.is_dir():
-            tree(child,depth+1)
+            tree(child, depth + 1)
         else:
-            print("    "*depth+str(child.name))
+            print("    " * depth + str(child.name))
+
 
 if __name__ == "__main__":
     tree(cwd)
