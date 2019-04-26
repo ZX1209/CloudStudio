799786d597db00f26e65f918e712030e165f48ae
GL <1404919041@qq.com>
2019-04-26 10:27:15 +0800

    better not capture_output as default;

--- bin/clara/ThirdShell.py
+++ bin/clara/ThirdShell.py
@@ -3,15 +3,17 @@ import shlex
 
 # todo: test?
 
+# note: usage runCmdStrs
 
-def runCmdStr(cmdstr, capture_output=True, **args):
+
+def runCmdStr(cmdstr, capture_output=False, **args):
     """
     cmdstr: str
     """
     return subprocess.run(shlex.split(cmdstr), capture_output=capture_output, **args)
 
 
-def runCmdStrs(cmdstrs, capture_output=True, **args):
+def runCmdStrs(cmdstrs, capture_output=False, **args):
     """
     cmds is like
     [
@@ -27,7 +29,7 @@ def runCmdStrs(cmdstrs, capture_output=True, **args):
     return rs
 
 
-def runCmdTrains(cmdstrs, capture_output=True, **args):
+def runCmdTrains(cmdstrs, capture_output=False, **args):
     """
     cmds is like
     [
@@ -57,5 +59,5 @@ def NoInternetConnected():
     1 for not connected
     0 for connected
     """
-    cp = subprocess.run(["ping", "-c", "1", "1.1.1.1"], capture_output=True)
+    cp = subprocess.run(["ping", "-c", "1", "1.1.1.1"], capture_output=False)
     return cp.returncode
