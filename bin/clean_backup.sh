e118b6d789aea8322e071427afba5fa33b0cfa2d
GL <1404919041@qq.com>
2021-02-25 09:54:57 +0800

    30 days is too short.

--- bin/clean_backup.sh
+++ bin/clean_backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-/usr/bin/find /home/gl/Archive/Category-backup/kde5/ -name "*--kde5_backup.zip" -type f -mtime +30 -exec rm -f {} \;
+/usr/bin/find /home/gl/Archive/Category-backup/kde5/ -name "*--kde5_backup.zip" -type f -mtime +120 -exec rm -f {} \;
 
-/usr/bin/find /home/gl/gl_home/archive/category-backup/calendars/ -name "*--calendars-backup" -type f -mtime +30 -exec rm -f {} \;
+/usr/bin/find /home/gl/gl_home/archive/category-backup/calendars/ -name "*--calendars-backup" -type f -mtime +120 -exec rm -f {} \;
